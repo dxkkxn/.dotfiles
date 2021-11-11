10836218ed55d0d01c381f88c512b2216ee2d7ee
dxkkxn <ybkstudies@gmail.com>
2021-11-11 13:48:39 +0100

    new vim config

--- linker.sh
+++ linker.sh
@@ -1,3 +1,5 @@
-ln -sf .dotfiles/.doom.d .doom.d
-ln -sf .dotfiles/.vim/vimrc .vimrc
-ln -sf .dotfiles/.zshrc .zshrc
+#!/bin/bash
+ln -sf ~/.dotfiles/.doom.d ~/.doom.d;
+ln -sf ~/.dotfiles/.vim/.vimrc ~/.vimrc;
+echo "Linked succesfully";
+#ln -sf .dotfiles/.zshrc .zshrc
