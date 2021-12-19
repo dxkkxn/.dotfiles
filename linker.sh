8f44399a9e8a514afbe815490776afa1c876ea49
dakkon <dakkon@pop-os.localdomain>
2021-12-19 13:54:28 +0100

    cleaning

--- linker.sh
+++ linker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -xe
 ln -sf ~/.dotfiles/.doom.d ~/.doom.d;
-#ln -sf ~/.dotfiles/.config/touchegg ~/.config/touchegg
+#ln -sf ~/.dotfiles/touchegg.conf ~/.config/touchegg/touchegg.conf
 
 #ln -sf ~/.dotfiles/.vim/.vimrc ~/.vimrc;
 ln -sf ~/.dotfiles/.SpaceVim.d ~/.SpaceVim.d;
 
 ln -sf ~/.dotfiles/.alacritty.yml ~/.alacritty.yml
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+#ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 echo "Linked succesfully";
